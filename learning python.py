d7fcec55d0b4ad2fd3245037a6065d96804311d9
Faye <yh41b@hotmail.com>
2021-10-09 19:07:15 -0500

    Making change to Python code
    Making change to Python code

---
+++ learning python.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+x = [1,2,3]
+y = [5, 6, 7]
+
+type(x)
+x+y
+array(x)
+import numpy as np
+np.array([x, y])
+x[0]
+x.count(x[0])
+print(False and True)
+
+#%% adding a new lines of code for testing in GitHub
+z = [1,2,3,4,5]
+z
+type(z)
+z = np.array(z)
